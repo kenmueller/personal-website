e88423b0097a6c8de79eb26fa4fe5388ef4e13ee
Ken Mueller <kenmueller0@gmail.com>
2020-05-26 01:27:12 -0700

    initial text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
+import Head from 'next/head'
+
+import useInitialText from '../hooks/useInitialText'
+
 import styles from '../styles/index.module.scss'
 
 export default () => {
+	const initialText = useInitialText()
+	
 	return (
 		<div className={styles.root}>
-			Hello
+			<Head>
+				<title>Ken Mueller</title>
+			</Head>
+			<p className={styles.initialText}>
+				{initialText}
+			</p>
 		</div>
 	)
 }
